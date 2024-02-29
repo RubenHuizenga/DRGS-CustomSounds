e52490cb0600d9deb1f553fd2a206fb272d4f85c
Ruben Huizenga <RubenHuizenga@proton.me>
2024-02-29 20:14:00 +0100

    Replace AudioSampleGroups

---
+++ CustomSounds/CustomSounds.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using CustomSounds.Patchers;
+using HarmonyLib;
+using Il2CppSystem.Text.RegularExpressions;
+using UnityEngine;
+using FundayFactory.Audio.Data;
+using NAudio.Wave;
+
+namespace CustomSounds
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class CustomSounds : BasePlugin
+    {
+        public static new ManualLogSource Log;
+        public static Dictionary<string, List<AudioSampleClip>> CustomSoundsReplace;
+
+        public override void Load()
+        {
+            Log = base.Log;
+
+            Harmony.CreateAndPatchAll(typeof(AudioSourceObjectPatcher));
+
+            Log.Log(LogLevel.Message, $"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+            CustomSoundsReplace = GetCustomAudioClips("Replace");
+        }
+
+        private static Dictionary<string, List<AudioSampleClip>> GetCustomAudioClips(string subPath)
+        {
+            var customSoundsPath = $@"{Paths.PluginPath}\CustomSounds\{subPath}";
+            var result = new Dictionary<string, List<AudioSampleClip>>();
+
+            if (Directory.Exists(customSoundsPath))
+            {
+                Log.Log(LogLevel.Info, $"Found folder {customSoundsPath}");
+
+                var wavFiles = Directory.GetFiles(customSoundsPath, "*.wav");
+
+                if (wavFiles.Length == 0)
+                {
+                    Log.Log(LogLevel.Warning, $"Could not find any .wav files in {customSoundsPath}");
+                    return result;
+                }
+
+                Log.Log(LogLevel.Info, $"Found {wavFiles.Length} .wav files in {customSoundsPath}");
+
+                foreach (var filePath in wavFiles)
+                {
+                    Log.Log(LogLevel.Debug, $"Processing file {filePath}");
+                    
+                    var fileKey = GetSoundKey(Path.GetFileNameWithoutExtension(filePath));
+
+                    var audioClip = LoadAudioClipFromWav(filePath, fileKey);
+
+                    if (!result.ContainsKey(fileKey))
+                    {
+                        Log.Log(LogLevel.Debug, $"Added new key '{fileKey}'");
+                        result.Add(fileKey, new List<AudioSampleClip>());
+                    }
+
+                    result[fileKey].Add(audioClip);
+                }
+
+                return result;
+            }
+
+            Log.Log(LogLevel.Fatal, $"Directory '{customSoundsPath}' does not exist");
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the Key from an input string. This can either be a <see cref="AudioSampleGroup.id"/> or a filepath (without extention)
+        /// Will trim any hierarchy and index numbers. e.g. "SFX/ui_button_hover_01" will turn into "ui_button_hover"
+        /// </summary>
+        public static string GetSoundKey(string input)
+        {
+            input = input.Split('/').Last();
+            return Regex.Replace(input, @"_\d*$", "");
+        }
+
+        public static AudioSampleClip LoadAudioClipFromWav(string path, string key)
+        {
+            using var audioFile = new AudioFileReader(path);
+
+            var audioData = new float[audioFile.Length / 4]; // Assuming 16-bit PCM
+            var bytesRead = audioFile.Read(audioData, 0, audioData.Length);
+
+            var audioClip = AudioClip.Create(key, bytesRead, audioFile.WaveFormat.Channels, audioFile.WaveFormat.SampleRate, false);
+            audioClip.SetData(audioData, 0);
+
+            return new AudioSampleClip() {clip = audioClip};
+        }
+    }
+}
---
+++ CustomSounds/Patchers/AudioSourceObjectPatcher.cs
@@ -0,0 +1,56 @@
+﻿using FundayFactory.Audio;
+using HarmonyLib;
+using System.Linq;
+using BepInEx.Logging;
+using FundayFactory.Audio.Data;
+using System.Collections.Generic;
+
+namespace CustomSounds.Patchers
+{
+    public class AudioSourceObjectPatcher
+    {
+        [HarmonyPatch(typeof(AudioSourceObject), "Play", typeof(AudioSampleGroup), typeof(bool))]
+        [HarmonyPrefix]
+        public static bool Play_Patch(ref AudioSampleGroup sampleGroup)
+        {
+            var key = CustomSounds.GetSoundKey(sampleGroup.id);
+
+            var replacementSounds = FindReplacementSounds(key);
+
+            if(!replacementSounds.Any())
+                return true;
+
+            if (replacementSounds.Any())
+                sampleGroup.clips = replacementSounds.ToArray();
+
+
+            CustomSounds.Log.Log(LogLevel.Message, $"Now playing '{sampleGroup.id}'. Count: {sampleGroup.clips.Count}");
+
+            // Reset the pitch from Random to One
+            // Voicelines are still pitchshifted, even with this line
+            // sampleGroup.pitch = new IAudioPitch(new AudioOnePitch().Pointer);
+
+            CustomSounds.Log.Log(LogLevel.Debug, $"Now playing '{sampleGroup.id}' using key '{key}'");
+
+            return true;
+        }
+
+        private static List<AudioSampleClip> FindReplacementSounds(string id)
+        {
+            var keys = CustomSounds.CustomSoundsReplace.Keys.Where(id.Equals).ToList();
+
+            if (!keys.Any())
+                return new List<AudioSampleClip>();
+
+            if (keys.Count > 1)
+            {
+                CustomSounds.Log.Log(LogLevel.Warning, $"Found multiple keys for samplegroup '{id}' in Replace group. Using the first one");
+                foreach (var key in keys)
+                    CustomSounds.Log.Log(LogLevel.Warning, $"    - {key}");
+            }
+
+            return CustomSounds.CustomSoundsReplace[keys.First()];
+        }
+
+    }
+}
